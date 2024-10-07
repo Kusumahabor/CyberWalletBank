fa50025bb04a4fd1ae2fb0d110135a165b7e2b4d
kusuma <kusuma.harbor@gmail.com>
2024-10-07 11:32:18 -0400

    Started building basic structure of Spring Boot and Thymeleaf project for Banking API

---
+++ src/main/java/com/cwbbank/CyberWalletBank/CyberWalletBankApplication.java
@@ -0,0 +1,13 @@
+package com.cwbbank.CyberWalletBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberWalletBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberWalletBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cwbbank/CyberWalletBank/CyberWalletBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cwbbank.CyberWalletBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CyberWalletBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
