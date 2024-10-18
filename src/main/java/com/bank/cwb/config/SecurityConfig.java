ea96533f0c454436d013ba541504310ed26ed456
kusuma <kusuma.harbor@gmail.com>
2024-10-18 14:45:47 -0400

    CyberWallet online banking project- Final1.4

--- src/main/java/com/bank/cwb/config/SecurityConfig.java
+++ src/main/java/com/bank/cwb/config/SecurityConfig.java
@@ -57,4 +57,5 @@ public class SecurityConfig {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(accountService).passwordEncoder(passwordEncoder());
     }
+
 }
